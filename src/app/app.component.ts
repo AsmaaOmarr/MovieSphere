d58dd1dc2e89bbd947df52138087a5885cf8a90d
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2025-03-01 22:49:05 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent, NavbarComponent],
+  imports: [RouterOutlet, ButtonComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
