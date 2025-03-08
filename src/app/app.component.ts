2b00c34f41b270189f9cad91397693cfd63cb840
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-09 00:45:13 +0200

    - Added plan interface - Added 2 new colors in app colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
