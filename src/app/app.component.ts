15b67b6862e1b6a537f18128126edc934b299c45
Arwa Ayman <aymanarwa05@gmail.com>
2025-03-09 20:51:25 +0200

    Auth logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { ButtonComponent } from './components/button/button.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HomeComponent } from "./components/home/home.component";
-import { RegisterComponent } from "./components/Auth/register/register.component";
-import { StartComponent } from "./components/Auth/start/start.component";
-import { SliderComponent } from "./components/Auth/slider/slider.component";
+
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterModule,
-    ButtonComponent,
-    NavbarComponent,
-    FooterComponent,
-    SidebarComponent,
-    HomeComponent,
-    RegisterComponent,
-    StartComponent,
-    SliderComponent
+   
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/components/Auth/login/login.component.ts
+++ src/app/components/Auth/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, Validators,ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
     encapsulation: ViewEncapsulation.None 
@@ -10,7 +14,35 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class LoginComponent {
   showPassword = false;
+  inValidUser=false;
+  user = { username: '', password: '' };
 
+  constructor(private authService: AuthService,private router:Router) {}
+loginFormValidation= new FormGroup({
+
+  name: new FormControl(null, [Validators.required, Validators.minLength(4)]),
+  password:new FormControl(null,[Validators.minLength(6),Validators.required])
+ },
+
+);
+get name (){
+  return this.loginFormValidation.get("name")
+}
+get password (){
+  return this.loginFormValidation.get("password")
+}
+  login() {
+    this.authService.login(this.user.username, this.user.password).subscribe(users =>
+       {
+      const validUser = users.find((u:any) => u.username === this.user.username && u.password === this.user.password);
+      if (validUser) {
+        localStorage.setItem('loggedInUser', JSON.stringify(validUser));
+        this.router.navigate(["/home"])
+      } else {
+       this.inValidUser=true;
+      }
+    });
+  }
  
  togglePassword() {
    this.showPassword = !this.showPassword;
