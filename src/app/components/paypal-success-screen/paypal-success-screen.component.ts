67f22d78247724cd9401c80c4a4ac80e9c860ead
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-09 16:14:46 +0200

    - Added sucess screen

---
+++ src/app/components/paypal-success-screen/paypal-success-screen.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { APP_COLORS } from '../../constants';
+
+@Component({
+  selector: 'app-paypal-success-screen',
+  imports: [CommonModule],
+  templateUrl: './paypal-success-screen.component.html',
+  styleUrl: './paypal-success-screen.component.css'
+})
+export class PaypalSuccessScreenComponent {
+    constructor(private router: Router) {}
+  
+    secondary: string = APP_COLORS.secondary;
+
+    navigateToHome() {
+        this.router.navigate(['/home']);
+    }
+}
