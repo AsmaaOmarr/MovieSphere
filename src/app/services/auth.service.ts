4158b4bf03b5a50b5984794e906d6a5a1a29738f
Arwa Ayman <aymanarwa05@gmail.com>
2025-03-21 15:06:25 +0200

    prevent duplicate usernames

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     return this.checkUsernameExists(user.username).pipe(
       map((usernameExists) => {
         if (usernameExists) {
-          throw new Error('Username is already taken');
+          throw new Error('Username is already taken. Please choose another one.');
         } else {
           return this.http.post(this.apiUrl, user);
         }
